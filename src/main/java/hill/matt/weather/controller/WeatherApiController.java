2ca0e1a2bdebdf5eb0baba2f8062f843dceb677f
brif <matthew0z@yahoo.com>
2025-12-28 22:22:02 +0800

    Update WeatherApiController

--- src/main/java/hill/matt/weather/controller/WeatherApiController.java
+++ src/main/java/hill/matt/weather/controller/WeatherApiController.java
@@ -1,11 +1,15 @@
 package hill.matt.weather.controller;
 
 import org.springdoc.core.annotations.ParameterObject;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import hill.matt.weather.model.ApiResponse;
 import hill.matt.weather.model.IncomingRequest;
 import hill.matt.weather.service.WeatherClientService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -15,8 +19,9 @@ public class WeatherApiController {
     private final WeatherClientService service;
 
     @GetMapping("/getweather")
-    public String getWeatherData(@ParameterObject IncomingRequest request) {
-        return new String("Great Success");
+    public ResponseEntity<ApiResponse> getWeatherData(@ParameterObject @Valid IncomingRequest request) {
+        return new ResponseEntity<>(service.retrieveWeatherForecase(request.latitude(), request.longitude()),
+                HttpStatus.OK);
     }
 
 }
