3204aef7416ce3ebbbf382b7c419d2fc044ebb8d
brif <matthew0z@yahoo.com>
2026-01-02 21:07:24 +0800

    Add cache config

---
+++ src/main/java/hill/matt/weather/config/CacheConfig.java
@@ -0,0 +1,27 @@
+package hill.matt.weather.config;
+
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+
+@Configuration
+public class CacheConfig {
+
+    @Bean
+    public CacheManager cacheManager() {
+        CaffeineCacheManager manager = new CaffeineCacheManager("weatherCache");
+        
+        manager.setCaffeine(
+                Caffeine.newBuilder()
+                        .expireAfterWrite(30, TimeUnit.MINUTES)
+                        .maximumSize(1_000));
+
+        return manager;
+    }
+
+}
