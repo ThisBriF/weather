dc403efc1100438eb504194392441044da895f34
brif <matthew0z@yahoo.com>
2026-01-03 00:42:32 +0800

    Remove cache enabling from main class

--- src/main/java/hill/matt/weather/WeatherApplication.java
+++ src/main/java/hill/matt/weather/WeatherApplication.java
@@ -3,11 +3,9 @@ package hill.matt.weather;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
-@EnableCaching
 public class WeatherApplication {
 
 	public static void main(String[] args) {
